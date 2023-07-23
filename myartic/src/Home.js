4cb1166d06fc96719bd4d7926721e0a69a0756ad
jb-vairoletto <mendez.r@hotmail.com>
2023-07-23 17:39:12 -0300

    feat(Home): added conditional pending and error messages

--- myartic/src/Home.js
+++ myartic/src/Home.js
@@ -4,19 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
     useEffect(()=>{
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if (!res.ok){
+                throw Error('could not fetch the data');
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
-        });
+            setError(null);
+            setIsPending(false);
+        })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
+        })  
     },[]);
 
     return ( 
         <div className="home" >
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading.....</div>}
             {blogs && <BlogList blogs={blogs}/>}
         </div>
      );
